ee8e89a0d1ff83b377fe6b2c1bb1d4eb047b1c67
Dee68 <ddimie283@gmail.com>
2023-02-24 22:38:40 +0000

    Add profile model test on creating user.

--- account/tests/test_models.py
+++ account/tests/test_models.py
@@ -1,1 +1,13 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from account.models import Profile
+
+
+class TestModels(TestCase):
+    def test_should_create_user_profile(self):
+        User = get_user_model()
+        user = User.objects.create_user(username='username', first_name='fname', last_name='lname', email='email@gmail.com', password='password')
+        user.set_password('password')
+        user.save()
+        pr = Profile.objects.get(user=user)
+        self.assertEqual(str(pr), f"{user.username}'s profile")
