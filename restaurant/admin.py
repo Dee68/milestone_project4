84a7812c71efada193510451be8acf7c709c8f3e
Dee68 <ddimie283@gmail.com>
2023-03-09 01:50:17 +0000

    resaturant.admin file corrected for syntax error.

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -5,7 +5,7 @@ from .models import Table, TableImage, Reservation
 class TableAdmin(admin.ModelAdmin):
     list_display = ['number', 'capacity', 'image_tag']
     readonly_fields = ['image_tag']
-    prepopulated_fields = {'slug':('title',)}
+    prepopulated_fields = {'slug': ('title',)}
 
 
 admin.site.register(Table, TableAdmin)
