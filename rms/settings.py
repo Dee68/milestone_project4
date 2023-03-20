6fb16c95561bd351beb441ab53f57e46c0a449e4
Dee68 <ddimie283@gmail.com>
2023-03-20 10:30:39 +0000

    Add correction to password reset functionality

--- rms/settings.py
+++ rms/settings.py
@@ -112,13 +112,17 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+if DEBUG:
+     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+     DEFAULT_FROM_EMAIL = 'info@gmail.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
