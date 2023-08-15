5526c47fef8a0e33979bb862352bda21d66fce18
Dee68 <ddimie283@gmail.com>
2023-08-15 22:50:42 +0000

    Add static missing files.

--- rms/settings.py
+++ rms/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['bongo-man.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['bongo-man.herokuapp.com', 'localhost',  '8000-dee68-milestoneproject-5miakvf1eh9.ws-eu103.gitpod.io']
 
 
 # Application definition
